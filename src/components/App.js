5814387b9e1f35931bb21ef93baec83d5cd12b41
udujoel <udujoel@gmail.com>
2019-12-24 12:07:21 +0100

    created app

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from './Header'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="catch-of-the-day">
+            <div className="menu">
+                <Header />
+                {/* <Inventory />
+                <Order /> */}
+
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
