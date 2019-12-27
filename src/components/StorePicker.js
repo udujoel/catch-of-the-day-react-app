95005c72aa382d8e631cbc1c0d06a754998bad66
udujoel <udujoel@gmail.com>
2019-12-27 09:21:23 +0100

    add default name fuction

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,11 +1,12 @@
 import React, { Fragment } from "react";
+import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
   render() {
     return (
       <form className="store-selector">
         <h2>Please Enter a Store</h2>
-        <input type="text" required placeholder="Store Name" />
+        <input type="text" required placeholder="Enter Store name" defaultValue = {getFunName()} />
         <button type="submit">Visit Store -></button>
       </form>
     );
