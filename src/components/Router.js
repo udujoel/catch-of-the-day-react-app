bd505982758bda0b4cf029fab0a39c2943796fee
udujoel <udujoel@gmail.com>
2019-12-27 08:59:47 +0100

    add router

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import StorePicker from "./StorePicker";
+import App from "./App";
 import React from "react";
 
 const Router = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={StorePicker} />
+      <Route exact path="/store/:storeId" component={App} />
     </Switch>
   </BrowserRouter>
 );
