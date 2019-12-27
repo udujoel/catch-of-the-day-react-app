ecc97c833339a0838c7796e3c1abf23d3cbe7e9f
udujoel <udujoel@gmail.com>
2019-12-27 09:10:04 +0100

    add 404 page

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,12 +2,14 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import StorePicker from "./StorePicker";
 import App from "./App";
 import React from "react";
+import NotFound from './NotFound'
 
 const Router = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={StorePicker} />
       <Route exact path="/store/:storeId" component={App} />
+      <Route component={NotFound} />
     </Switch>
   </BrowserRouter>
 );
