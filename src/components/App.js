bf977fb7dca897990a9032947bb3ffc40d5e09b6
udujoel <udujoel@gmail.com>
2019-12-29 19:14:17 +0100

    pass function betweeen pages

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./Header";
 import Inventory from "./Inventory";
 import Order from "./Order";
+import sampleFishes from "../sample-fishes";
 
 class App extends React.Component {
   state = {
@@ -19,6 +20,11 @@ class App extends React.Component {
       fishes: fishes
     });
   };
+
+  loadSampleFishes = () => {
+    this.setState({ fishes: sampleFishes });
+  };
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -26,7 +32,10 @@ class App extends React.Component {
           <Header tagline="Fresh SeaFood Market" />
         </div>
         <Order />
-        <Inventory addFish={this.addFish} />
+        <Inventory
+          loadSampleFishes={this.loadSampleFishes}
+          addFish={this.addFish}
+        />
       </div>
     );
   }
