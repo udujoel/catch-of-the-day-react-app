d928ef357de42e90174ec97e4e7dcab6a6f692db
udujoel <udujoel@gmail.com>
2019-12-30 20:46:17 +0100

    ternary operator for sold-out button

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -13,7 +13,9 @@ class Fish extends React.Component {
           <span className="price">{formatPrice(price)}</span>
         </h3>
         <p>{desc}</p>
-        <button disabled={!isAvailable}>Add to Cart</button>
+        <button disabled={!isAvailable}>
+          {isAvailable ? "Add to Cart" : "Sold Out!"}
+        </button>
       </li>
     );
   }
