67a2a17173ee0438607ba308b7b561999180897d
udujoel <udujoel@gmail.com>
2019-12-30 20:41:29 +0100

    fixed availability

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -4,6 +4,7 @@ import { formatPrice } from "../helpers";
 class Fish extends React.Component {
   render() {
     const { name, desc, image, price, status } = this.props.detail;
+    const isAvailable = status === "available";
     return (
       <li className="menu-fish">
         <img src={image} alt={name} />
@@ -12,7 +13,7 @@ class Fish extends React.Component {
           <span className="price">{formatPrice(price)}</span>
         </h3>
         <p>{desc}</p>
-        <button>Add to Cart</button>
+        <button disabled={!isAvailable}>Add to Cart</button>
       </li>
     );
   }
