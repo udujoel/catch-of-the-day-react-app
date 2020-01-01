baa822ea62184c9ddc052c7a9949790a2de06387
udujoel <udujoel@gmail.com>
2020-01-02 00:05:07 +0100

    getting total order

--- src/components/Order.js
+++ src/components/Order.js
@@ -3,12 +3,18 @@ import React from "react";
 class Order extends React.Component {
   render() {
     const orderIds = Object.keys(this.props.order);
-    const total = orderIds.reduce((acc,cur)=>{},0)
+    const total = orderIds.reduce((acc, key) => {
+      const fish = this.props.fishes[key];
+      const count = this.props.order[key];
+
+      count ? acc += fish.price : acc += fish.price * count;
+      return acc;
+    }, 0);
     return (
       <div className="inventory">
         <h2>Order</h2>
         <ul>
-          <li>{console.log(this.props.order)}</li>
+          <li>{total}</li>
         </ul>
       </div>
     );
